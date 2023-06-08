e55295076c9f6139b6d57f1b0ef568c3f0943566
evans <kumarshiva0707@gmail.com>
2023-06-08 23:58:51 +0530

    updated count -ve and +ve

--- June/count_of_positive_int.py
+++ June/count_of_positive_int.py
@@ -1,1 +1,11 @@
 #  counts no of positive and negative integer
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        count1 = 0
+        count2 = 0
+        for i in nums:
+            if i < 0:
+                count1 += 1
+            if i > 0:
+                count2 += 1
+        return max(count1, count2)
