2dfdad8c53791fdf0ca424b8b7a8f11d75b9ceaf
evans <kumarshiva0707@gmail.com>
2023-06-09 23:21:30 +0530

    updated Find Smallest Letter Greater Than Target

--- June/Find Smallest Letter Greater Than Target.py
+++ June/Find Smallest Letter Greater Than Target.py
@@ -1,1 +1,9 @@
 # Find Smallest Letter Greater Than Target
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        
+        for i in letters:
+            if ord(i[0]) > ord(target[0]):
+                return i
+        return letters[0]
