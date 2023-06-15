5c9cbd7be78827435fb151a0c835bfb7c2f3db0e
evans <kumarshiva0707@gmail.com>
2023-06-15 22:20:20 +0530

    max level sum of binary tree

---
+++ June/max_level_sum_binarytree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        
+        def bfs(root, arr):
+            q = []
+            q.append(root)
+            
+            while len(q):
+                temp = []
+                l = len(q)
+                for i in range(l):
+                    x = q.pop(0)
+                    temp.append(x.val)
+                    if x.left:
+                        q.append(x.left)
+                    if x.right:
+                        q.append(x.right)
+                arr.append(temp)
+        arr = []
+        # ans = []
+        bfs(root, arr)
+        print(arr)
+        maxm = sum(arr[0])
+        ind = 0
+        for i in range(len(arr)):
+            if sum(arr[i]) > maxm:
+                maxm = sum(arr[i])
+                ind = i
+            
+            # ans.append(maxm)
+        return ind+1
